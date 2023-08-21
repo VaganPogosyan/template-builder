6751fc1ab319e02168faac5a9fb0602b12118f14
Vagan Pogosyan <vagan2502@gmail.com>
2023-08-21 01:04:10 -0400

    changed to use fs module to set up a folder and traverse through its files

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const path = require("path");
+const fs = require("fs/promises");
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
@@ -8,13 +9,48 @@ function createMainWindow() {
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   mainWindow.loadFile(path.join(__dirname, "./src/index.html"));
 }
 
+async function handleFolderOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ["openDirectory"],
+  });
+
+  let filesInFolder;
+
+  if (!canceled) {
+    // return filePaths[0];
+    const files = await fs.readdir(filePaths[0]);
+
+    // const files = await fs.readdir(filePaths[0], (err, files) => {
+    //   if (err) {
+    //     console.error("Error reading folder:", err);
+    //     return;
+    //   }
+
+    //   console.log("Files in the folder:");
+    //   console.log(files);
+    //   // return files;
+    //   // filesInFolder = [...files];
+
+    //   // files.forEach((file) => {
+    //   //   filesInFolder.push(file);
+    //   // });
+    // });
+    return [...files];
+    // console.log("f: " + filesInFolder);
+    // return filesInFolder;
+  }
+}
+
 app.whenReady().then(() => {
+  ipcMain.handle("dialog:openFolder", handleFolderOpen);
+
   createMainWindow();
 
   if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,53 +1,64 @@
 const submitButton = document.querySelector("#submitButton");
 const clearButton = document.querySelector("#clearButton");
-const firstCodeInput = document.querySelector("#firstCode");
-const secondCodeInput = document.querySelector("#secondCode");
+const openFolderButton = document.querySelector("#openFolderButton");
+const procedureCodeInput = document.querySelector("#procedureCode");
+const denialCodeInput = document.querySelector("#denialCode");
 const form = document.querySelector("#form");
 const errors = document.querySelector("#errors");
+const fileInput = document.querySelector("#fileInput");
+const textAreaForProcedureCode = document.querySelector("#textareaOutput");
 
-const firstCodeDB = ["234", "567", "C57", "X21"];
-const secondCodeDB = ["11", "RF", "AB", "4F", "9P"];
+// const procedureCodeDB = ["234", "567", "C57", "X21"];
+// const denialCodeDB = ["11", "RF", "AB", "4F", "9P"];
 // const combinations = { 1: ["234", "AB"], 2: ["RF", "234"], 3: ["X21", "9P"] };
-const combinations = { "234+RF": "fff", "234+AB": "ffx", "C57+9P": "ffa" };
+// const combinations = { "234+RF": "fff", "234+AB": "ffx", "C57+9P": "ffa" };
 // {'234+AB': templateCode, '234+RF': templateCode}
 
+function outputForProcedureCode(procedureCode, patientName) {
+  const textForProcedureCode = `Hello ${patientName} this a to warn you about procedure code: ${procedureCode}`;
+}
+
 function checkCodes() {
-  const firstCode = firstCodeInput.value.toString().toUpperCase().trim();
-  const secondCode = secondCodeInput.value.toString().toUpperCase().trim();
+  const procedureCode = procedureCodeInput.value
+    .toString()
+    .toUpperCase()
+    .trim();
+  const denialCode = denialCodeInput.value.toString().toUpperCase().trim();
   let noErrors = true;
 
-  if (!firstCodeDB.includes(firstCode) && firstCode !== "") {
+  if (!procedureCodeDB.includes(procedureCode) && procedureCode !== "") {
     showError("First code doesn't exist");
     noErrors = false;
   }
-  if (!secondCodeDB.includes(secondCode) && secondCode !== "") {
+  if (!denialCodeDB.includes(denialCode) && denialCode !== "") {
     showError("Second code doesn't exist");
     noErrors = false;
   }
-  if (firstCode == "") {
+  if (procedureCode == "") {
     showError("Please enter first code");
     noErrors = false;
   }
-  if (secondCode == "") {
+  if (denialCode == "") {
     showError("Please enter second code");
     noErrors = false;
   }
 
   if (noErrors) {
-    const templateCode = checkCombination(firstCode, secondCode);
-    if (!templateCode) return false;
-    console.log(`TEMPLATE CODE: ${templateCode}`);
+    // const templateCode = checkCombination(procedureCode, denialCode);
+    // if (!templateCode) return false;
+    // console.log(`TEMPLATE CODE: ${templateCode}`);
+    // showTemplateCode(templateCode);
   } else {
     return noErrors;
   }
 }
 
-// function showError(text) {
-//   const errorText = document.createElement("h1");
-//   errorText.style.color = "red";
-//   errorText.innerText = text;
-//   errors.appendChild(errorText);
-// }
+function showTemplateCode(templateCode) {
+  const errorText = document.createElement("h1");
+  errorText.style.color = "green";
+  errorText.innerText = templateCode;
+  errors.appendChild(errorText);
+}
 
 function showError(text) {
   const errorText = document.createElement("h1");
@@ -57,16 +68,32 @@ function showError(text) {
 }
 
 // check the what is the combination of two codes
-function checkCombination(firstCode, secondCode) {
+function checkCombination(procedureCode, denialCode) {
   // Object.keys(combinations);
-  const combinedCode = `${firstCode}+${secondCode}`;
+  const combinedCode = `${procedureCode}+${denialCode}`;
   console.log(combinedCode);
   if (!combinations[combinedCode]) {
-    showError("Teamplate doesn't exist");
+    showError("Template doesn't exist");
     return;
   }
   return combinations[combinedCode];
 }
+
+function handleFile(event) {
+  const file = event.target.files[0]; // Get the first selected file
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const contents = e.target.result;
+      errors.innerText = contents;
+    };
+
+    reader.readAsText(file); // Read the file as text
+  }
+}
+
+// fileInput.addEventListener("change", handleFile);
 
 clearButton.addEventListener("click", (e) => {
   location.reload();
@@ -81,9 +108,15 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-firstCodeInput.addEventListener("keydown", () => {
+openFolderButton.addEventListener("click", async () => {
+  const files = await window.electronAPI.openFolder();
+  errors.innerText = files;
+  console.log(files);
+});
+
+procedureCodeInput.addEventListener("keydown", () => {
   errors.innerText = "";
 });
-secondCodeInput.addEventListener("keydown", () => {
+denialCodeInput.addEventListener("keydown", () => {
   errors.innerText = "";
 });
